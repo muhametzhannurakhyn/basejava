5d384fcbea7068b579afb3b668e7d773083998c5
Muhametzhan <nurakhyn04@gmail.com>
2025-01-12 23:17:21 +0500

    1Change

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,52 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-
+    Resume[] storage = new Resume[100];
+    int size = 0;
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+
+        }
+        System.out.println("Резюме с таким uuid не найдено.");
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[size - 1]; // Перемещаем последнее резюме на место удаляемого
+                storage[size - 1] = null; // Удаляем ссылку на последний элемент
+                size--; // Уменьшаем размер
+                return;
+            }
+        }
+        System.out.println("Резюме с таким uuid не найдено.");
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
+        Resume[] result = new Resume[size];
+
         return new Resume[0];
     }
 
     int size() {
-        return 0;
+        return this.size;
     }
 }
